f7f2a14c5130a20c8a4bf9339c0ed1b57a780a5c
Владимир Астапенко <vladimir@MacBookAir.lan>
2025-05-05 07:37:59 +0300

    fixed the tests

---
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
@@ -0,0 +1,41 @@
+package mobi.sevenwinds.app.budget
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object BudgetService {
+    suspend fun addRecord(body: BudgetRecord): BudgetRecord = withContext(Dispatchers.IO) {
+        transaction {
+            val entity = BudgetEntity.new {
+                this.year = body.year
+                this.month = body.month
+                this.amount = body.amount
+                this.type = body.type
+            }
+
+            return@transaction entity.toResponse()
+        }
+    }
+
+    suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse = withContext(Dispatchers.IO) {
+        transaction {
+            val query = BudgetTable
+                .select { BudgetTable.year eq param.year }
+                .orderBy(BudgetTable.month to SortOrder.ASC, BudgetTable.amount to SortOrder.DESC)
+
+            val total = query.count()
+            val data = BudgetEntity.wrapRows(query).map { it.toResponse() }
+
+            val sumByType = data.groupBy { it.type.name }.mapValues { it.value.sumOf { v -> v.amount } }
+
+            return@transaction BudgetYearStatsResponse(
+                total = total,
+                totalByType = sumByType,
+                items = data.asSequence().drop(param.offset).take(param.limit).toList()
+            )
+        }
+    }
+}

bfdf6636c594812a3088891a7034ef1460cb28b6
Владимир Астапенко <vladimir@MacBookAir.lan>
2025-05-05 07:46:14 +0300

    change in expense commission

--- src/main/resources/db/migration/V2_Сommission.sql
+++ src/main/resources/db/migration/V2_Сommission.sql
@@ -1,8 +1,1 @@
-create table budget
-(
-    id     serial primary key,
-    year   int  not null,
-    month  int  not null,
-    amount int  not null,
-    type   text not null
-);
+update budget set type = 'Расход' where type = 'Комиссия';
